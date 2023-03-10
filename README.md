95a0ad02fa8cbfd66815da7304c2a717ad5fac7f
spencer-rafada <spencer.byui@gmail.com>
2023-03-09 23:12:25 -0700

    boilerplate for the project

--- README.md
+++
@@ -1,2 +0,0 @@
-# things-to-do-in-Rexubrg
-This is the frontend repository for Things to Do in Rexburg site. JavaScript, CSS, HTML, Netlify.
---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  root: './src/',
+  build: {
+    outDir: '../dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html')
+      }
+    }
+  }
+});
