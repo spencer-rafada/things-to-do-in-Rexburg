ae071e2f8ba3c81674f490b7bdd52c04a98c143a
spencer-rafada <spencer.byui@gmail.com>
2023-03-15 18:24:35 -0600

    added header and footer on every page.

---
+++ src/js/utils.mjs
@@ -0,0 +1,23 @@
+const loadTemplate = async (path) => {
+  const response = await fetch(path);
+  const template = response.text();
+  return template;
+};
+
+const renderWithTemplate = (template, parentElement, position, data, callback) => {
+  parentElement.innerHTML = ``;
+  parentElement.insertAdjacentHTML(position, template);
+  if (callback) {
+    callback(data);
+  }
+};
+
+export const loadHeaderFooter = async () => {
+  const footer = await loadTemplate('../public/partials/footer.html');
+  const header = await loadTemplate('../public/partials/header.html');
+  const footerElement = document.querySelector('footer');
+  const headerElement = document.querySelector('header');
+
+  renderWithTemplate(header, headerElement, 'afterbegin');
+  renderWithTemplate(footer, footerElement, 'afterbegin');
+};
