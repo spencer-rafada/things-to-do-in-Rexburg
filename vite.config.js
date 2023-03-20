7fcfc9b4a1ff00e0bae751a8f59437ad80397641
spencer-rafada <spencer.byui@gmail.com>
2023-03-20 11:50:27 -0600

    refactored config file

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,12 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'src/index.html'),
-        favorites: resolve(__dirname, 'src/favorites/index.html')
+        favorites: resolve(__dirname, 'src/favorites/index.html'),
+        'activity-details': resolve(__dirname, 'src/activity-details/index.html'),
+        'add-activity': resolve(__dirname, 'src/add-activity/index.html'),
+        admin: resolve(__dirname, 'src/admin/index.html'),
+        developers: resolve(__dirname, 'src/developers/index.html'),
+        profile: resolve(__dirname, 'src/profile/index.html')
       }
     }
   }
